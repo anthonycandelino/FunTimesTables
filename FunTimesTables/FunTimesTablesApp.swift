255029b0c106ca4d3fda194a773a15009a2788f6
Anthony Candelino <anthcandelino@gmail.com>
2024-06-27 14:46:25 -0400

    Initial Commit

---
+++ FunTimesTables/FunTimesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunTimesTablesApp.swift
+//  FunTimesTables
+//
+//  Created by Anthony Candelino on 2024-06-27.
+//
+
+import SwiftUI
+
+@main
+struct FunTimesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
